7ef5aabcd0341a16d7ec9304c16a835966a71049
Tim Hanewich <tahanewich@live.com>
2020-07-06 19:05:35 -0400

    Tested button input

--- GpioTest.py
+++ GpioTest.py
@@ -2,9 +2,11 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(21, GPIO.OUT)
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.output(21, True)
-time.sleep(1)
-GPIO.output(21, False)
+while True:
+	print(GPIO.input(12))
+	time.sleep(1)
+
+
 print("Done")
