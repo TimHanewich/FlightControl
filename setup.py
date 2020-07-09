5d1f59129f774c9d491856516a237f0e7345769f
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-07-09 10:47:57 -0400

    Added markdown to longdescription in setup

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='flightcontrol',
       version='0.1.0',
       description='Flight tracking resources',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/TimHanewich/FlightControl',
       author='Tim Hanewich',
       author_email='tahanewich@live.com',
       license='MIT',
-      packages=['flightcontrol'],
+      packages=setuptools.find_packages(),
       zip_safe=False)
